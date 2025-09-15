42c900bcb27b57fe8de1193a5160137fae116a2a
Manuel Martínez <juanma_codes@JuanMa.localdomain>
2025-09-15 13:09:17 -0600

    favorite pokemons

--- src/store/providers.tsx
+++ src/store/providers.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Provider } from 'react-redux';
 import { store } from './';
 
@@ -8,6 +8,14 @@ interface Props {
 }
 
 export const Providers = ({children}: Props) => {
+
+  useEffect(() => {
+    // console.log('Providers - useEffect')
+    const favorites = JSON.parse( localStorage.getItem('favorite-pokemons') || '{}' );
+    console.log({favorites})
+    store.dispatch( {type: 'pokemons/setFavoritePokemons', payload: favorites} )
+  }, [])
+
   return (
     <Provider store={store}>
         {children}
